56962c86076f19cfe625d96f3e68a7427f17b760
prateeksarangi <sarangiprateek80@gmail.com>
2019-07-17 23:01:17 +0530

    Sum added

---
+++ Java/Sum.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class Sum{
+    public static void main(String[] args){
+        var a = 4;
+        var b = 9;
+        System.out.println("Sum of a and b is " + (a+b));
+    }
+}
