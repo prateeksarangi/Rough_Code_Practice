71de79d46a5ba4d4ad1b3b22b744f232a6480a98
prateeksarangi <sarangiprateek80@gmail.com>
2020-04-05 23:41:43 +0530

    CPP files added

---
+++ CPP_Codes/FindDigit.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<math.h> 
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int i, n;
+	double s = 0;
+	cin>>n;
+
+	if (n < 0) 
+        return 0; 
+  
+    if (n <= 1) 
+        return 1;
+    else{
+		for(i=2; i<=n; i++){
+			s += log10(i);
+		}
+
+		cout<<floor(s) + 1;
+	}
+	return 0;
+}
